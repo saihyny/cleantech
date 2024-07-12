014a1b4db9b34ff4511c230965b369a250a30fb6
saihyny <satyasaikiranrocks@gmail.com>
2024-07-12 13:09:47 +0530

    hh

--- src/components/coursel.js
+++ src/components/coursel.js
@@ -53,6 +53,7 @@ export default function Carousel({
         <div className="flex items-center justify-center gap-2">
           {slides.map((_, i) => (
             <div
+              key={i}
               className={`
               transition-all w-3 h-3 bg-white rounded-full
               ${curr === i ? "p-2" : "bg-opacity-50"}
